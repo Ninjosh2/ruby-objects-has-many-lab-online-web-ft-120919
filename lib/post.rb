8243978af0ceb17357806a9a44951074c535c3fd
Joshua Sanders <ninjosh2@gmail.com>
2020-01-07 05:57:43 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,3 +1,16 @@
-class Post 
-  attr_accessor :title 
+class Post
+  attr_accessor :author, :title 
+
+  def initialize(title)
+    @title = title
+  end
+
+  def author_name
+    if author 
+      self.author.name 
+    else
+      nil
+    end
+  end
+
 end 
